e0cda5b7990d47f7dfd43c71231001fbe1846699
GuiPD1610 <156451203+GuiPD1610@users.noreply.github.com>
2024-12-30 20:36:28 -0300

    Aula 30/12

---
+++ C_aula1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+/*Variaveis
+Tipos de variaveis:
+    int 
+    float
+    double
+    char
+    string
+    bool
+    void
+Definição:
+    #define var 10
+    int var = 10;
+*/
+
+
+
+    return 0;
+}
